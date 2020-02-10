dbdda158a5241adb7e720aba2dd12638fe26f9a7
kbenoit <kbenoit@lse.ac.uk>
2020-02-11 09:14:56 +1100

    Add default method tests

---
+++ tests/testthat/test-default-methods.R
@@ -0,0 +1,35 @@
+context("test default methods for nice error messages")
+
+test_that("test default textmodel methods", {
+    expect_error(
+        textmodel_affinity(TRUE, FALSE),
+        "textmodel_affinity\\(\\) only works on dfm.*objects"
+    )
+    expect_error(
+        textmodel_ca(TRUE),
+        "textmodel_ca\\(\\) only works on dfm.*objects"
+    )
+    expect_error(
+        textmodel_nb(TRUE),
+        "textmodel_nb\\(\\) only works on dfm.*objects"
+    )
+    expect_error(
+        textmodel_wordfish(TRUE),
+        "textmodel_wordfish\\(\\) only works on dfm.*objects"
+    )
+    expect_error(
+        textmodel_wordscores(TRUE),
+        "textmodel_wordscores\\(\\) only works on dfm.*objects"
+    )
+})
+
+test_that("test default textplot methods", {
+    expect_error(
+        textplot_influence(TRUE),
+        "textplot_influence\\(\\) only works on influence\\..*textmodel_affinity objects"
+    )
+    expect_error(
+        textplot_scale1d(TRUE),
+        "textplot_scale1d\\(\\) only works on predict\\.textmodel.*objects"
+    )
+})
