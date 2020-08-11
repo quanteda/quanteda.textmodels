8c2e06c53f1c81857c5f66b1090765c0ef40558b
Kohei Watanabe <watanabe.kohei@gmail.com>
2020-08-11 13:50:40 +0200

    Improve the test

--- tests/testthat/test-textmodel_lda.R
+++ tests/testthat/test-textmodel_lda.R
@@ -1,13 +1,14 @@
 context("test textmodel_lda")
 
 require(quanteda)
-toks <- tokens(data_corpus_moviereviews[1:20], 
+toks <- tokens(data_corpus_moviereviews[1:500], 
                remove_punct = TRUE, 
                remove_symbols = TRUE,
                remove_number = TRUE)
 dfmt <- dfm(toks) %>% 
-        dfm_remove(stopwords(), min_nchar = 2) %>% 
-        dfm_trim(max_docfreq = 5)
+    dfm_remove(stopwords(), min_nchar = 2) %>% 
+    dfm_trim(max_docfreq = 0.1, docfreq_type = "prop")
+sifi <- c("space", "mars", "alien", "earth")
 
 test_that("LDA is working", {
     
@@ -16,20 +17,16 @@ test_that("LDA is working", {
     
     expect_equal(dim(terms(lda, 10)), c(10, 5))
     expect_equal(dim(terms(lda, 20)), c(20, 5))
-    expect_identical(
-        terms(lda)[1:2,],
-        matrix(
-            c("joe", "gorilla", "stalked", "obvious", "zero", "alicia", 
-              "cool", "baldwin", "mars", "ghosts"), nrow = 2,
-            dimnames = list(
-                NULL, 
-                c("topic1", "topic2", "topic3", "topic4", "topic5")
-            )
-        )
+    expect_equal(
+        colnames(terms(lda)),
+        c("topic1", "topic2", "topic3", "topic4", "topic5")
     )
-    expect_identical(
-       topics(lda)[1:5],
-        c("topic2", "topic5", "topic4", "topic4", "topic2")
+    expect_true(
+        sum(apply(terms(lda), 2, function(x)  all(sifi %in% x))) == 1 # there is a sifi topic
+    )
+    expect_setequal(
+       topics(lda), 
+       c("topic1", "topic2", "topic3", "topic4", "topic5")
     )
     expect_equal(
         ncol(terms(textmodel_lda(dfmt, k = 1))), 1
@@ -43,7 +40,7 @@ test_that("LDA is working", {
     )
     expect_output(
         print(lda),
-        "Topics: 5; 20 documents; 2996 features."
+        "Topics: 5; 500 documents; 22605 features."
     )
 })
 
@@ -57,19 +54,22 @@ test_that("seeded LDA is working", {
     
     expect_equal(dim(terms(lda, 10)), c(10, 3))
     expect_equal(dim(terms(lda, 20)), c(20, 3))
-    expect_identical(
-        terms(lda)[1:2,],
-        matrix(
-            c("couple", "love", "star", "space", "cool", "baldwin"), nrow = 2,
-            dimnames = list(
-                NULL, 
-                c("romance", "sifi", "other")
-            )
-        )
+    expect_equal(
+        colnames(terms(lda)),
+        c("romance", "sifi", "other")
     )
-    expect_identical(
-        topics(lda)[1:5],
-        c("sifi", "sifi", "other", "other", "sifi")
+    expect_false(
+        any(sifi %in% terms(lda)[,"romance"])    
+    )
+    expect_true(
+        all(sifi %in% terms(lda)[,"sifi"])
+    )
+    expect_false(
+        any(sifi %in% terms(lda)[,"other"])    
+    )
+    expect_setequal(
+        topics(lda),
+        c("romance", "sifi", "other")
     )
     expect_equal(
         ncol(terms(textmodel_seededlda(dfmt, dict, residual = FALSE))), 2
@@ -84,6 +84,6 @@ test_that("seeded LDA is working", {
     )
     expect_output(
         print(lda),
-        "Topics: 3; 20 documents; 2996 features."
+        "Topics: 3; 500 documents; 22605 features."
     )
 })
