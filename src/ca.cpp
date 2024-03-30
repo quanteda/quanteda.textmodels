9b1c65cdaef7e1346d1a21583282cff1d464bb57
Kohei Watanabe <watanabe.kohei@gmail.com>
2024-03-30 18:17:19 +0900

    Remove parallel logic from cpp_ca()

--- src/ca.cpp
+++ src/ca.cpp
@@ -4,8 +4,12 @@ using namespace arma;
 
 
 //find the principle elements for the sparse residual matrix
-void create_residual_ca(std::size_t row_num, const arma::sp_mat& dfm, const arma::colvec &rsum, const arma::rowvec &csum,
-                        const double residual_floor, const std::size_t K, Triplets &residual_tri)
+void create_residual_ca(std::size_t row_num, 
+                        const arma::sp_mat& dfm, 
+                        const arma::colvec &rsum, 
+                        const arma::rowvec &csum,
+                        const double residual_floor, 
+                        const std::size_t K, Triplets &residual_tri)
 {
     for (std::size_t k = 0; k < K; k++){
         double residual = (dfm(row_num, k) - rsum(row_num) * csum(k)) / sqrt(rsum(row_num) * csum(k) );
@@ -18,8 +22,7 @@ void create_residual_ca(std::size_t row_num, const arma::sp_mat& dfm, const arma
 
 // [[Rcpp::export]]
 S4 cpp_ca(const arma::sp_mat &dfm, 
-          const double residual_floor,
-          const int threads = -1){
+          const double residual_floor){
     
     const std::size_t N = dfm.n_rows;
     const std::size_t K = dfm.n_cols;
@@ -32,41 +35,12 @@ S4 cpp_ca(const arma::sp_mat &dfm,
     Triplets residual_tri;
     residual_tri.reserve(N * K / 1000); // assume 99.9% sparsity
 
-#if QUANTEDA_USE_TBB
-    tbb::task_arena arena(thread);
-    arena.execute([&]{
-        tbb::parallel_for(tbb::blocked_range<int>(0, N), [&](tbb::blocked_range<int> r) {
-            for (int i = r.begin(); i < r.end(); ++i) {
-                create_residual_ca(i, dfm, rsum, csum, residual_floor, K, residual_tri);
-            }
-        });
-    });
-#else
     for (std::size_t i = 0; i < N; i++) {
         create_residual_ca(i, dfm, rsum, csum, residual_floor, K, residual_tri);
     }
-#endif
-    
+
     return to_matrix( residual_tri, N, K, false );
-//     std::size_t residual_size = residual_tri.size();
-//     IntegerVector dim_ = IntegerVector::create(N, K);
-//     IntegerVector i_(residual_size), j_(residual_size);
-//     NumericVector x_(residual_size);
-//
-//     for (std::size_t k = 0; k < residual_tri.size(); k++) {
-//         i_[k] = std::get<0>(residual_tri[k]);
-//         j_[k] = std::get<1>(residual_tri[k]);
-//         x_[k] = std::get<2>(residual_tri[k]);
-//     }
-//
-//     S4 res_("dgTMatrix");
-//     res_.slot("i") = i_;
-//     res_.slot("j") = j_;
-//     res_.slot("x") = x_;
-//     res_.slot("Dim") = dim_;
-//
-//     return res_;
-    
+
 }
 
 /***R
