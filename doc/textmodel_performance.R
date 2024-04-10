7264fb8e8a7db74e3dfeaf5ed6cc811156fb5de5
Kenneth Benoit <kbenoit@KB-MacBook-Pro-14.local>
2024-04-10 20:18:10 +0100

    For resbumission of 0.9.7

--- doc/textmodel_performance.R
+++ doc/textmodel_performance.R
@@ -1,27 +1,48 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
+  error = TRUE,
   collapse = TRUE,
   comment = "##"
 )
 
 ## ----setup--------------------------------------------------------------------
-library("quanteda.textmodels")
 library("quanteda")
+library("quanteda.textmodels")
 
-## -----------------------------------------------------------------------------
+## ----echo = FALSE-------------------------------------------------------------
 # large movie review database of 50,000 movie reviews
-load(url("https://www.dropbox.com/s/sjdfmx8ggwfda5o/data_corpus_LMRD.rda?dl=1"))
+data_corpus_LMRD <- NULL
+suppressWarnings(
+    try(load(url("https://quanteda.org/data/data_corpus_LMRD.rda")))
+)
+if (is.null(data_corpus_LMRD)) {
+    data_corpus_LMRD <- data_corpus_moviereviews
+    data_corpus_LMRD$polarity <- data_corpus_LMRD$sentiment
+    data_corpus_LMRD$set <- "train"
+    data_corpus_LMRD$set[c(sample(1:1000, size = 200),
+                           sample(1001:2000, size = 200))] <- "test"
+}
 
-dfmat <- dfm(data_corpus_LMRD)
+dfmat <- tokens(data_corpus_LMRD) %>%
+  dfm()
 dfmat_train <- dfm_subset(dfmat, set == "train")
 dfmat_test <- dfm_subset(dfmat, set == "test")
+
+## ----eval = FALSE-------------------------------------------------------------
+#  # large movie review database of 50,000 movie reviews
+#  load(url("https://quanteda.org/data/data_corpus_LMRD.rda"))
+#  
+#  dfmat <- tokens(data_corpus_LMRD) %>%
+#    dfm()
+#  dfmat_train <- dfm_subset(dfmat, set == "train")
+#  dfmat_test <- dfm_subset(dfmat, set == "test")
 
 ## -----------------------------------------------------------------------------
 library("microbenchmark")
 microbenchmark(
     multi = textmodel_nb(dfmat_train, dfmat_train$polarity, distribution = "multinomial"),
     bern = textmodel_nb(dfmat_train, dfmat_train$polarity, distribution = "Bernoulli"),
-    times = 50
+    times = 20
 )
 
 ## -----------------------------------------------------------------------------
@@ -30,7 +51,7 @@ microbenchmark(
                     newdata = dfmat_test),
     bern = predict(textmodel_nb(dfmat_train, dfmat_train$polarity, distribution = "Bernoulli"),
                    newdata = dfmat_test),
-    times = 50
+    times = 20
 )
 
 ## -----------------------------------------------------------------------------
@@ -50,7 +71,7 @@ microbenchmark(
       tmod = multinomial_naive_bayes(as(dfmat_train, "dgCMatrix"), dfmat_train$polarity, laplace = 1)
       pred <- predict(tmod, newdata = as(dfmat_test, "dgCMatrix"))
     },
-    times = 50
+    times = 20
 )
 
 ## -----------------------------------------------------------------------------
@@ -58,7 +79,7 @@ dfmat_train_bern <- dfm_weight(dfmat_train, scheme = "boolean")
 dfmat_test_bern <- dfm_weight(dfmat_test, scheme = "boolean")
 
 microbenchmark(
-    textmodels = {
+    textmodel_nb = {
       tmod <-  textmodel_nb(dfmat_train_bern, dfmat_train$polarity, smooth = 1, distribution = "Bernoulli")
       pred <- predict(tmod, newdata = dfmat_test)
     },
@@ -70,6 +91,6 @@ microbenchmark(
       tmod = bernoulli_naive_bayes(as(dfmat_train_bern, "dgCMatrix"), dfmat_train$polarity, laplace = 1)
       pred <- predict(tmod, newdata = as(dfmat_test_bern, "dgCMatrix"))
     },
-    times = 50
+    times = 20
 )
 
